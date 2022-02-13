029b0c0e1cd09cc707a7b36711a8be0b11a3c245
md_dell <jlkmh82@msn.com>
2022-02-13 11:26:07 +0900

    lesson_3.Hook&State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <br/>
-      <Content />
-    </div>
-  );
+  const [number, setNumber] = useState(0);
+
+  function onClickPlus() {
+    setNumber(number + 1);
+  }
+
+  function onClickMinus() {
+    setNumber(number - 1);
+  }
+
+  function onClickMultiply() {
+    setNumber(number * 2);
+  }
+
+  function onClickInit() {
+    setNumber(0);
+  }
+
+
+    return (
+        <div>
+            <div>값 : {number}</div>
+            <button onClick={onClickInit}>0</button>
+            <button onClick={onClickPlus}>+1</button>
+            <button onClick={onClickMinus}>-1</button>
+            <button onClick={onClickMultiply}>*2</button>
+        </div>
+    );
 }
 
 export default App;
