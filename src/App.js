b8c3185a8f608ff54a0b29bd18045a5e9210786e
md_dell <jlkmh82@msn.com>
2022-02-15 23:22:24 +0900

    lesson_6-1. data init

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-import React, { useState } from 'react';
-import Button from './Button';
-import Layout from './Layout';
+import './App.css';
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  function onClickPlus() {
-    setNumber(number + 1);
-  }
-
-  function onClickMinus() {
-    setNumber(number - 1);
-  }
-
-  function onClickMultiply() {
-    setNumber(number * 2);
-  }
-
-  function onClickInit() {
-    setNumber(0);
-  }
-
-  return (
-    <Layout>
-        <div>{number} * 1 = {number * 1}</div>
-        <div>{number} * 2 = {number * 2}</div>
-        <div>{number} * 3 = {number * 3}</div>
-        <div>{number} * 4 = {number * 4}</div>
-        <div>{number} * 5 = {number * 5}</div>
-        <div>{number} * 6 = {number * 6}</div>
-        <div>{number} * 7 = {number * 7}</div>
-        <div>{number} * 8 = {number * 8}</div>
-        <div>{number} * 9 = {number * 9}</div>
-
-        <Button onClick={onClickInit} text="0" />
-        <Button onClick={onClickPlus} text="+1" />
-        <Button onClick={onClickMinus} text="-1" />
-        <Button onClick={onClickMultiply} text="*2" />
-    </Layout>
-  );
+  return <div>시작</div>;
 }
 
 export default App;
--- src/Header.js
+++
@@ -1,7 +0,0 @@
-import styles from './Header.module.css'
-
-function Header() {
-  return <div className={styles.title}>Travel Diary</div>;
-}
-
-export default Header;
