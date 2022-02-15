1b016412f618e62ea2922c9e01120a81201ce675
md_dell <jlkmh82@msn.com>
2022-02-16 00:22:08 +0900

    lesson_6-2. youtubeData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
+import youtubeData from './data/youtubeData.json';
 
 function App() {
-  return <div>시작</div>;
+  console.log('youtubeData : ', youtubeData);
+  console.log('youtubeData[data]', youtubeData['data']);
+  console.log('youtubeData[data][0]', youtubeData['data'][0]);
+  console.log('id : ', youtubeData['data'][0].id, ' -> 영상의 링크 주소');
+  console.log('title : ', youtubeData['data'][0].title, ' -> 영상 제목');
+  console.log('viewCount : ', youtubeData['data'][0].viewCount, ' -> 조회수');
+
+  return <div>리액트로 데이터 불러오기</div>;
+  
 }
 
 export default App;
